096fd020e635d659085ac385f0c3060f97810fc7
zhangzhihao <zhihao2030@163.com>
2024-07-03 16:16:05 +0800

    add

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
       {/*  · Status*/}
       {/*</Link>*/}
       <div className="flex items-center gap-3 mt-3 sm:ml-3 sm:mt-0 md:hidden text-sm text-gray-300">
-        <Link to="/about">{t("About")}</Link>
+        {/*<Link to="/about">{t("About")}</Link>*/}
         {/*<Link to="/privacy">{t("Privacy")}</Link>*/}
         {/*<Link to="/terms">{t("Terms")}</Link>*/}
         {/*<Link to="https://status.wr.do" target="_blank" rel="noreferrer">*/}
