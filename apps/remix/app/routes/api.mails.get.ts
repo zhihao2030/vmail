baead6ee44d777b5ec4e8cb4ec02a8114811a4ae
zhzhang <zzh0660@gmail.com>
2025-06-24 09:43:59 +0800

    lowcase

--- apps/remix/app/routes/api.mails.get.ts
+++ apps/remix/app/routes/api.mails.get.ts
@@ -6,18 +6,19 @@ export const loader: LoaderFunction = async ({ request }) => {
   // 从URL获取查询参数
   const url = new URL(request.url);
   const mailbox = url.searchParams.get("mailbox");
-  
   if (!mailbox) {
     return new Response("Missing mailbox parameter", { status: 400 });
   }
-
+  //@ 分割， 第一一部分转小写
+  const mailboxArray = mailbox.split("@");
+  const res = mailboxArray[0].toLowerCase() + "@" + mailboxArray[1];
   const db = getWebTursoDB(
     process.env.TURSO_DB_URL as string,
     process.env.TURSO_DB_RO_AUTH_TOKEN as string
   );
 
   try {
-    const mails = await getEmailsByMessageTo(db, mailbox);
+    const mails = await getEmailsByMessageTo(db, res);
     return mails;
   } catch (error) {
     console.error("Error fetching emails:", error);
