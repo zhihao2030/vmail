0e51760c115a80a04c97eeafe53fe60d79827431
zhzhang <zzh0660@gmail.com>
2025-10-22 11:28:16 +0800

    1

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -61,8 +61,8 @@ export default function App() {
           }}></script> */}
     </head>
     <body className="">
-    <noscript dangerouslySetInnerHTML={{
-      __html: '<iframe title="tag manager" src="https://www.googletagmanager.com/ns.html?id=GTM-KVZSVVD" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
+    <div dangerouslySetInnerHTML={{
+      __html: '<noscript><iframe title="tag manager" src="https://www.googletagmanager.com/ns.html?id=GTM-KVZSVVD" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'
     }} />
     <Outlet/>
     <ScrollRestoration/>
